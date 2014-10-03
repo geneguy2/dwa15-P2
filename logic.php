aee022149df5611f90e66896bd30aa8291123481
Gene Guy <gene@geneguy.com>
2014-10-02 23:18:00 -0400

    Updated the logic.php file

--- logic.php
+++ logic.php
@@ -2,7 +2,7 @@
 /*
 Create arrays for the words, and symbols
 */
-$words = array("one", "weather", "two", "burn", "twice", "phone", "mints", "cap", "remote", "paper", "book", "pen", "pencil", "red", "Karen", "dust");
+$words = array("one", "weather", "two", "burn", "twice", "phone", "mints", "cap", "remote", "paper", "book", "pen", "pencil", "red", "plane", "dust", "car", "train");
 $symbolsList = array("#", "@", "*", "!", "$", "&");
 $numbers = array("1", "2", "3", "4", "5");
 $password  = array();
